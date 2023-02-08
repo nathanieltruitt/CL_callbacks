82a7c39a67d2b40bb53b6eeb03e7c76a895e4ffa
Nathaniel Truitt <nathantruitt2@gmail.com>
2023-02-08 16:01:18 -0600

    ran migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  before_create :post_being_created
+  after_create :post_has_been_created
+
+  private
+
+  def post_being_created
+    puts 'Post is being created'
+  end
+
+  def post_has_been_created
+    puts 'Post has been created'
+  end
 end
